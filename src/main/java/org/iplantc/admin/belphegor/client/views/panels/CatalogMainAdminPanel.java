9eaf3ba788c06ab38683c12a013d9e7d708d99d8
sriram srinivasan <sriram@sriram-srinivasans-mac-pro.local>
2011-12-12 09:07:23 -0700

    view classes for admin interface

---
+++ src/main/java/org/iplantc/admin/belphegor/client/views/panels/CatalogMainAdminPanel.java
@@ -0,0 +1,13 @@
+package org.iplantc.admin.belphegor.client.views.panels;
+
+import org.iplantc.core.uiapplications.client.services.AppTemplateServiceFacade;
+import org.iplantc.core.uiapplications.client.views.panels.BaseCatalogMainPanel;
+
+public class CatalogMainAdminPanel extends BaseCatalogMainPanel {
+
+    public CatalogMainAdminPanel(AppTemplateServiceFacade templateService) {
+        super(templateService);
+        // TODO Auto-generated constructor stub
+    }
+
+}
