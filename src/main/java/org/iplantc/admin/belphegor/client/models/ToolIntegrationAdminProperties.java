1b33fdb6b5cc2ebb5b6b928181bbf15bffeb93bc
Paul Sarando <psarando@iplantcollaborative.org>
2011-12-14 12:59:40 -0700

    Added service endpoint URLs for admin functions.
    Added mock service endpoints in belphegor.properties and access methdos in ToolIntegrationAdminProperties.
    Removed obsolete mule service URLs.
    Added initial service methods that utilize these endpoints in AppTemplateAdminServiceFacade.

--- src/main/java/org/iplantc/admin/belphegor/client/models/ToolIntegrationAdminProperties.java
+++ src/main/java/org/iplantc/admin/belphegor/client/models/ToolIntegrationAdminProperties.java
@@ -1,10 +1,14 @@
 package org.iplantc.admin.belphegor.client.models;
 
+import java.util.Arrays;
 import java.util.Map;
+
+import com.extjs.gxt.ui.client.core.FastMap;
 
 public class ToolIntegrationAdminProperties {
 
     private static ToolIntegrationAdminProperties instance;
+    private FastMap<String> serviceUrlMap;
 
     /**
      * The prefix used in each of the property names.
@@ -12,39 +16,56 @@ public class ToolIntegrationAdminProperties {
     private static final String PROPERTY_NAME_PREFIX = "org.iplantc.belphegor."; //$NON-NLS-1$
 
     /**
-     * The base URL used to access the Mule services.
+     * The URL used to access the App Groups service.
      */
-    private static final String UNPROTECTED_MULE_SERVICE_BASE_URL = PROPERTY_NAME_PREFIX
-            + "unprotectedMuleServiceBaseUrl"; //$NON-NLS-1$
+    private static final String SERVICE_URL_CATEGORY_LIST = PROPERTY_NAME_PREFIX + "get-app-groups"; //$NON-NLS-1$
 
     /**
-     * The base URL used to access the Mule services.
+     * The URL used to access the Apps by Group service.
      */
-    private static final String MULE_SERVICE_BASE_URL = PROPERTY_NAME_PREFIX + "muleServiceBaseUrl"; //$NON-NLS-1$
+    private static final String SERVICE_URL_CATEGORY_APPS = PROPERTY_NAME_PREFIX + "get-apps-in-group"; //$NON-NLS-1$
+
+    /**
+     * The URL used to access the Add Category service.
+     */
+    private static final String SERVICE_URL_CATEGORY_ADD = PROPERTY_NAME_PREFIX + "add-category"; //$NON-NLS-1$
+
+    /**
+     * The URL used to access the Rename Category service.
+     */
+    private static final String SERVICE_URL_CATEGORY_RENAME = PROPERTY_NAME_PREFIX + "rename-category"; //$NON-NLS-1$
+
+    /**
+     * The URL used to access the Move Category service.
+     */
+    private static final String SERVICE_URL_CATEGORY_MOVE = PROPERTY_NAME_PREFIX + "move-category"; //$NON-NLS-1$
+
+    /**
+     * The URL used to access the Delete Category service.
+     */
+    private static final String SERVICE_URL_CATEGORY_DELETE = PROPERTY_NAME_PREFIX + "delete-category"; //$NON-NLS-1$
+
+    /**
+     * The URL used to access the Update App service.
+     */
+    private static final String SERVICE_URL_APP_UPDATE = PROPERTY_NAME_PREFIX + "update-app"; //$NON-NLS-1$
+
+    /**
+     * The URL used to access the Move App service.
+     */
+    private static final String SERVICE_URL_APP_MOVE = PROPERTY_NAME_PREFIX + "move-app"; //$NON-NLS-1$
+
+    /**
+     * The URL used to access the Delete App service.
+     */
+    private static final String SERVICE_URL_APP_DELETE = PROPERTY_NAME_PREFIX + "delete-app"; //$NON-NLS-1$
 
     /**
      * Properties key of the context click enabled option
      */
-
     private static final String CONTEXT_CLICK_ENABLED = PROPERTY_NAME_PREFIX + "contextMenu.enabled";//$NON-NLS-1$
 
-    /**
-     * The base URL used to access the DE Unprotected Mule services.
-     */
-    private String unproctedMuleServiceBaseUrl;
-
-    private String muleServiceBaseUrl;
-
     private boolean contextClickEnabled;
-
-    /**
-     * Gets the base URL used to access the DE Mule services.
-     * 
-     * @return the URL as a string.
-     */
-    public String getMuleServiceBaseUrl() {
-        return muleServiceBaseUrl;
-    }
 
     public static ToolIntegrationAdminProperties getInstance() {
 
@@ -55,27 +76,19 @@ public class ToolIntegrationAdminProperties {
     }
 
     /**
-     * @return the unproctedMuleServiceBaseUrl
-     */
-    public String getUnproctedMuleServiceBaseUrl() {
-        return unproctedMuleServiceBaseUrl;
-    }
-
-    /**
-     * @return the contextClickEnabled
-     */
-    public boolean isContextClickEnabled() {
-        return contextClickEnabled;
-    }
-
-    /**
      * Initializes this class from the given set of properties.
      * 
      * @param properties the properties that were fetched from the server.
      */
     public void initialize(Map<String, String> properties) {
-        muleServiceBaseUrl = properties.get(MULE_SERVICE_BASE_URL);
-        unproctedMuleServiceBaseUrl = properties.get(UNPROTECTED_MULE_SERVICE_BASE_URL);
+        serviceUrlMap = new FastMap<String>();
+
+        for (String key : Arrays.asList(SERVICE_URL_CATEGORY_ADD, SERVICE_URL_CATEGORY_RENAME,
+                SERVICE_URL_CATEGORY_MOVE, SERVICE_URL_CATEGORY_DELETE, SERVICE_URL_CATEGORY_LIST,
+                SERVICE_URL_CATEGORY_APPS, SERVICE_URL_APP_UPDATE, SERVICE_URL_APP_MOVE,
+                SERVICE_URL_APP_DELETE)) {
+            serviceUrlMap.put(key, properties.get(key));
+        }
 
         try {
             contextClickEnabled = Boolean.parseBoolean(properties.get(CONTEXT_CLICK_ENABLED));
@@ -84,4 +97,91 @@ public class ToolIntegrationAdminProperties {
         }
     }
 
+    /**
+     * Gets the Add Category service URL.
+     * 
+     * @return the URL as a string.
+     */
+    public String getAddCategoryServiceUrl() {
+        return serviceUrlMap.get(SERVICE_URL_CATEGORY_ADD);
+    }
+
+    /**
+     * Gets the Rename Category service URL.
+     * 
+     * @return the URL as a string.
+     */
+    public String getRenameCategoryServiceUrl() {
+        return serviceUrlMap.get(SERVICE_URL_CATEGORY_RENAME);
+    }
+
+    /**
+     * Gets the Move Category service URL.
+     * 
+     * @return the URL as a string.
+     */
+    public String getMoveCategoryServiceUrl() {
+        return serviceUrlMap.get(SERVICE_URL_CATEGORY_MOVE);
+    }
+
+    /**
+     * Gets the Delete Category service URL.
+     * 
+     * @return the URL as a string.
+     */
+    public String getDeleteCategoryServiceUrl() {
+        return serviceUrlMap.get(SERVICE_URL_CATEGORY_DELETE);
+    }
+
+    /**
+     * Gets the Category Listing service URL.
+     * 
+     * @return the URL as a string.
+     */
+    public String getCategoryListServiceUrl() {
+        return serviceUrlMap.get(SERVICE_URL_CATEGORY_LIST);
+    }
+
+    /**
+     * Gets the Apps-by-Category service URL.
+     * 
+     * @return the URL as a string.
+     */
+    public String getAppsInCategoryServiceUrl() {
+        return serviceUrlMap.get(SERVICE_URL_CATEGORY_APPS);
+    }
+
+    /**
+     * Gets the Update App service URL.
+     * 
+     * @return the URL as a string.
+     */
+    public String getUpdateAppServiceUrl() {
+        return serviceUrlMap.get(SERVICE_URL_APP_UPDATE);
+    }
+
+    /**
+     * Gets the Move App service URL.
+     * 
+     * @return the URL as a string.
+     */
+    public String getMoveAppServiceUrl() {
+        return serviceUrlMap.get(SERVICE_URL_APP_MOVE);
+    }
+
+    /**
+     * Gets the Delete App service URL.
+     * 
+     * @return the URL as a string.
+     */
+    public String getDeleteAppServiceUrl() {
+        return serviceUrlMap.get(SERVICE_URL_APP_DELETE);
+    }
+
+    /**
+     * @return the contextClickEnabled
+     */
+    public boolean isContextClickEnabled() {
+        return contextClickEnabled;
+    }
 }
--- src/main/java/org/iplantc/admin/belphegor/client/services/AppTemplateAdminServiceFacade.java
+++ src/main/java/org/iplantc/admin/belphegor/client/services/AppTemplateAdminServiceFacade.java
@@ -4,13 +4,18 @@ import org.iplantc.admin.belphegor.client.models.ToolIntegrationAdminProperties;
 import org.iplantc.core.uiapplications.client.services.AppTemplateServiceFacade;
 import org.iplantc.de.shared.services.ServiceCallWrapper;
 
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.json.client.JSONString;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 public class AppTemplateAdminServiceFacade implements AppTemplateServiceFacade {
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public void getAnalysisCategories(String workspaceId, AsyncCallback<String> callback) {
-        String address = ToolIntegrationAdminProperties.getInstance().getUnproctedMuleServiceBaseUrl()
-                + "get-only-analysis-groups" + "/1"; //$NON-NLS-1$
+        String address = ToolIntegrationAdminProperties.getInstance().getCategoryListServiceUrl();
         ServiceCallWrapper wrapper = new ServiceCallWrapper(address);
         ToolIntegrationAdminServiceFacade.getInstance().getServiceData(wrapper, callback);
     }
@@ -20,11 +25,122 @@ public class AppTemplateAdminServiceFacade implements AppTemplateServiceFacade {
      */
     @Override
     public void getAnalysis(String analysisGroupId, AsyncCallback<String> callback) {
-        String address = ToolIntegrationAdminProperties.getInstance().getMuleServiceBaseUrl()
-                + "get-analyses-in-group/" //$NON-NLS-1$
+        String address = ToolIntegrationAdminProperties.getInstance().getAppsInCategoryServiceUrl() + "?" //$NON-NLS-1$
                 + analysisGroupId;
         ServiceCallWrapper wrapper = new ServiceCallWrapper(address);
         ToolIntegrationAdminServiceFacade.getInstance().getServiceData(wrapper, callback);
     }
 
+    /**
+     * Adds a new Category with the given category name.
+     * 
+     * @param name
+     * @param callback
+     */
+    public void addCategory(String name, AsyncCallback<String> callback) {
+        String address = ToolIntegrationAdminProperties.getInstance().getAddCategoryServiceUrl() + "?" //$NON-NLS-1$
+                + name;
+
+        ServiceCallWrapper wrapper = new ServiceCallWrapper(address);
+        ToolIntegrationAdminServiceFacade.getInstance().getServiceData(wrapper, callback);
+    }
+
+    /**
+     * Renames a Category with the given category ID to the given name.
+     * 
+     * @param categoryId
+     * @param name
+     * @param callback
+     */
+    public void renameCategory(String categoryId, String name, AsyncCallback<String> callback) {
+        String address = ToolIntegrationAdminProperties.getInstance().getRenameCategoryServiceUrl();
+
+        JSONObject body = new JSONObject();
+        body.put("categoryId", new JSONString(categoryId)); //$NON-NLS-1$
+        body.put("name", new JSONString(name)); //$NON-NLS-1$
+
+        ServiceCallWrapper wrapper = new ServiceCallWrapper(ServiceCallWrapper.Type.POST, address,
+                body.toString());
+        ToolIntegrationAdminServiceFacade.getInstance().getServiceData(wrapper, callback);
+    }
+
+    /**
+     * Moves a Category with the given category ID to a parent Category with the given parentCategoryId.
+     * 
+     * @param categoryId
+     * @param parentCategoryId
+     * @param callback
+     */
+    public void moveCategory(String categoryId, String parentCategoryId, AsyncCallback<String> callback) {
+        String address = ToolIntegrationAdminProperties.getInstance().getMoveCategoryServiceUrl();
+
+        JSONObject body = new JSONObject();
+        body.put("categoryId", new JSONString(categoryId)); //$NON-NLS-1$
+        body.put("parent_categoryId", new JSONString(parentCategoryId)); //$NON-NLS-1$
+
+        ServiceCallWrapper wrapper = new ServiceCallWrapper(ServiceCallWrapper.Type.POST, address,
+                body.toString());
+        ToolIntegrationAdminServiceFacade.getInstance().getServiceData(wrapper, callback);
+    }
+
+    /**
+     * Deletes the Category with the given category ID.
+     * 
+     * @param categoryId
+     * @param callback
+     */
+    public void deleteCategory(String categoryId, AsyncCallback<String> callback) {
+        String address = ToolIntegrationAdminProperties.getInstance().getDeleteCategoryServiceUrl()
+                + "?" + categoryId; //$NON-NLS-1$
+
+        ServiceCallWrapper wrapper = new ServiceCallWrapper(address);
+        ToolIntegrationAdminServiceFacade.getInstance().getServiceData(wrapper, callback);
+    }
+
+    /**
+     * Updates an app with the given values in application.
+     * 
+     * @param application
+     * @param callback
+     */
+    public void updateApplication(JSONObject application, AsyncCallback<String> callback) {
+        String address = ToolIntegrationAdminProperties.getInstance().getUpdateAppServiceUrl();
+
+        ServiceCallWrapper wrapper = new ServiceCallWrapper(ServiceCallWrapper.Type.POST, address,
+                application.toString());
+        ToolIntegrationAdminServiceFacade.getInstance().getServiceData(wrapper, callback);
+    }
+
+    /**
+     * Moves an App with the given applicationId to the category with the given groupId.
+     * 
+     * @param applicationId
+     * @param groupId
+     * @param callback
+     */
+    public void moveApplication(String applicationId, String groupId, AsyncCallback<String> callback) {
+        String address = ToolIntegrationAdminProperties.getInstance().getMoveAppServiceUrl();
+
+        JSONObject body = new JSONObject();
+        body.put("app_id", new JSONString(applicationId)); //$NON-NLS-1$
+        body.put("group_id", new JSONString(groupId)); //$NON-NLS-1$
+
+        ServiceCallWrapper wrapper = new ServiceCallWrapper(ServiceCallWrapper.Type.POST, address,
+                body.toString());
+        ToolIntegrationAdminServiceFacade.getInstance().getServiceData(wrapper, callback);
+    }
+
+    /**
+     * Deletes an App with the given applicationId.
+     * 
+     * @param applicationId
+     * @param callback
+     */
+    public void deleteApplication(String applicationId, AsyncCallback<String> callback) {
+        String address = ToolIntegrationAdminProperties.getInstance().getDeleteAppServiceUrl() + "?" //$NON-NLS-1$
+                + applicationId;
+
+        ServiceCallWrapper wrapper = new ServiceCallWrapper(address);
+        ToolIntegrationAdminServiceFacade.getInstance().getServiceData(wrapper, callback);
+    }
 }
