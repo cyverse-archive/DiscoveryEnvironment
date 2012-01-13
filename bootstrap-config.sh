fa3776d3648a995de76e7453b9b200a28fcf7772
Andrew Lenards <andrew.lenards@gmail.com>
2012-01-13 15:49:01 -0700

    Provide local dev configuration bootstrapping.

---
+++ bootstrap-config.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+CONFIGULON="../configulon"
+
+if [[ ! -e $CONFIGULON ]]; then
+    echo "ERROR: A clone of configulon much be available at "$CONFIGULON
+    exit 1
+fi
+
+if [[ ! -d $1 ]]; then
+    DE_ENV="de-1"
+    echo "...No development environment indicated, using 'de-1'"
+else 
+    DE_ENV=$1
+    echo "...development environment set to "$DE_ENV
+fi
+
+PROP_FILE="discoveryenvironment.properties"
+SEC_FILE="de-security.properties"
+
+TARGET_PATH="./src/main/resources/"
+
+if [[ ! -e $TARGET_PATH ]]; then
+    echo "ERROR: Target path "$TARGET_PATH" does not exist." 
+    exit 1
+fi
+
+cp $CONFIGULON"/dev/"$DE_ENV"/"$PROP_FILE $TARGET_PATH
+cp $CONFIGULON"/dev/"$DE_ENV"/"$SEC_FILE $TARGET_PATH"security.properties"
+
+echo "...bootstrapped!" 
