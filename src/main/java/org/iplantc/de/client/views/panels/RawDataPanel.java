cyverse-archive/DiscoveryEnvironment

commit d4106034acb9695d8540e66fc62d061008170925

Clean up RawData-related services and UI widgets.

src/main/java/org/iplantc/de/client/views/panels/RawDataPanel.java
@@ -3,31 +3,17 @@ package org.iplantc.de.client.views.panels;
 import java.util.List;
 import java.util.Map;
 
-import org.iplantc.core.uicommons.client.ErrorHandler;
-import org.iplantc.core.uicommons.client.events.EventBus;
-import org.iplantc.core.uicommons.client.views.dialogs.IPlantDialog;
 import org.iplantc.core.uidiskresource.client.models.File;
 import org.iplantc.core.uidiskresource.client.models.FileIdentifier;
 import org.iplantc.de.client.I18N;
 import org.iplantc.de.client.controllers.DataMonitor;
-import org.iplantc.de.client.events.FileEditorWindowDirtyEvent;
-import org.iplantc.de.client.services.RawDataServices;
-import org.iplantc.de.client.utils.NotifyInfo;
 
-import com.extjs.gxt.ui.client.event.ButtonEvent;
-import com.extjs.gxt.ui.client.event.Events;
-import com.extjs.gxt.ui.client.event.FieldEvent;
-import com.extjs.gxt.ui.client.event.Listener;
-import com.extjs.gxt.ui.client.event.SelectionListener;
 import com.extjs.gxt.ui.client.widget.ContentPanel;
 import com.extjs.gxt.ui.client.widget.MessageBox;
-import com.extjs.gxt.ui.client.widget.button.Button;
 import com.extjs.gxt.ui.client.widget.form.TextArea;
 import com.extjs.gxt.ui.client.widget.layout.FitLayout;
-import com.extjs.gxt.ui.client.widget.toolbar.ToolBar;
 import com.google.gwt.json.client.JSONObject;
 import com.google.gwt.user.client.Element;
-import com.google.gwt.user.client.rpc.AsyncCallback;
 
 /**
  * Provides a user interface for presenting raw data.
@@ -39,7 +25,6 @@ public class RawDataPanel extends ProvenanceContentPanel implements DataMonitor 
     private final String data;
     private TextArea areaData;
     private String textOrig = new String();
-    private final boolean editable;
     private final MessageBox wait;
     private int tabIndex;
 
@@ -50,11 +35,10 @@ public class RawDataPanel extends ProvenanceContentPanel implements DataMonitor 
      * @param data data to display.
      * @param editable true if the user can edit this data.
      */
-    public RawDataPanel(FileIdentifier fileIdentifier, String data, boolean editable) {
+    public RawDataPanel(FileIdentifier fileIdentifier, String data) {
         super(fileIdentifier);
         this.data = data;
-        this.editable = editable;
-
+        
         wait = MessageBox.wait(I18N.DISPLAY.progress(), I18N.DISPLAY.fileSaveProgress(),
                 I18N.DISPLAY.saving() + "..."); //$NON-NLS-1$
         wait.close();
@@ -65,90 +49,7 @@ public class RawDataPanel extends ProvenanceContentPanel implements DataMonitor 
     }
 
     private void buildTextArea() {
-        areaData = buildTextArea(editable);
         areaData.setId("idRawDataField"); //$NON-NLS-1$
-
-        // we don't need to listen for changes if we are not editable
-        if (editable) {
-            areaData.addListener(Events.OnKeyUp, new Listener<FieldEvent>() {
-                @Override
-                public void handleEvent(FieldEvent be) {
-                    String text = areaData.getValue();
-
-                    if (!text.equals(textOrig)) {
-                        textOrig = text;
-
-                        // don't fire event if we are already dirty
-                        if (!dirty) {
-                            dirty = true;
-                            EventBus eventbus = EventBus.getInstance();
-                            FileEditorWindowDirtyEvent event = new FileEditorWindowDirtyEvent(
-                                    fileIdentifier.getFileId(), true);
-                            eventbus.fireEvent(event);
-                        }
-                    }
-                }
-            });
-        }
-    }
-
-    private void doSave() {
-        if (areaData != null) {
-            String body = areaData.getValue();
-
-            if (fileIdentifier != null) {
-                wait.show();
-                RawDataServices.saveRawData(fileIdentifier.getFileId(), fileIdentifier.getFilename(),
-                        body, new AsyncCallback<String>() {
-                            @Override
-                            public void onSuccess(String result) {
-                                EventBus eventbus = EventBus.getInstance();
-                                FileEditorWindowDirtyEvent event = new FileEditorWindowDirtyEvent(
-                                        fileIdentifier.getFileId(), false);
-                                eventbus.fireEvent(event);
-                                wait.close();
-                                NotifyInfo.display(I18N.DISPLAY.save(), I18N.DISPLAY.fileSave());
-                            }
-
-                            @Override
-                            public void onFailure(Throwable caught) {
-                                ErrorHandler.post(I18N.ERROR.rawDataSaveFailed(), caught);
-                                wait.close();
-                            }
-                        });
-            }
-        }
-    }
-
-    private void promptSaveAs() {
-        IPlantDialog dlg = new IPlantDialog(I18N.DISPLAY.saveAs(), 340, new RawDataSaveAsDialogPanel(
-                fileIdentifier, areaData.getValue(), wait));
-        dlg.show();
-    }
-
-    private ToolBar buildToolbar() {
-        ToolBar ret = new ToolBar();
-        final int TOOLBAR_HEIGHT = 24;
-
-        ret.setWidth(getWidth());
-        ret.setHeight(TOOLBAR_HEIGHT);
-
-        ret.add(new Button(I18N.DISPLAY.save(), new SelectionListener<ButtonEvent>() {
-            @Override
-            public void componentSelected(ButtonEvent ce) {
-                doSave();
-            }
-        }));
-
-        // add our Save As button
-        ret.add(new Button(I18N.DISPLAY.saveAs(), new SelectionListener<ButtonEvent>() {
-            @Override
-            public void componentSelected(ButtonEvent ce) {
-                promptSaveAs();
-            }
-        }));
-
-        return ret;
     }
 
     /**
@@ -168,10 +69,6 @@ public class RawDataPanel extends ProvenanceContentPanel implements DataMonitor 
             panel.setLayout(new FitLayout());
             panel.setWidth(getWidth());
             panel.add(areaData);
-
-            if (editable) {
-                panel.setTopComponent(buildToolbar());
-            }
 
             add(panel, centerData);
         }

src/main/java/org/iplantc/de/client/services/RawDataSaveAsCallback.java
@@ -1,79 +1,0 @@
-package org.iplantc.de.client.services;
-
-import org.iplantc.core.jsonutil.JsonUtil;
-import org.iplantc.core.uicommons.client.ErrorHandler;
-import org.iplantc.de.client.I18N;
-import org.iplantc.de.client.factories.EventJSONFactory;
-import org.iplantc.de.client.utils.MessageDispatcher;
-
-import com.extjs.gxt.ui.client.widget.MessageBox;
-import com.google.gwt.json.client.JSONObject;
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-/**
- * Defines an asynchronous callback for raw data "save as" events.
- * 
- * @author amuir
- * 
- */
-public class RawDataSaveAsCallback implements AsyncCallback<String> {
-    private String idParent;
-    private String idOrig;
-    private MessageBox wait;
-
-    /**
-     * Instantiate from a parent folder id, original file id and message box.
-     * 
-     * @param idParent parent folder id.
-     * @param idOrig original file identifier.
-     * @param wait message box to display while waiting.
-     */
-    public RawDataSaveAsCallback(String idParent, String idOrig, MessageBox wait) {
-        this.idParent = idParent;
-        this.idOrig = idOrig;
-        this.wait = wait;
-
-        if (wait != null) {
-            wait.show();
-        }
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void onFailure(Throwable caught) {
-        ErrorHandler.post(I18N.ERROR.rawDataSaveFailed(), caught);
-        wait.close();
-    }
-
-    private String buildPayload(final String json) {
-        StringBuffer ret = new StringBuffer();
-
-        ret.append("{"); //$NON-NLS-1$
-
-        if (json != null) {
-            JSONObject jsonObj = JsonUtil.getObject(json);
-            ret.append("\"created\": " + jsonObj.get("created").toString() + ", "); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-            ret.append("\"idParent\": \"" + idParent + "\", "); //$NON-NLS-1$ //$NON-NLS-2$
-            ret.append("\"idOrig\": \"" + idOrig + "\""); //$NON-NLS-1$ //$NON-NLS-2$
-        }
-
-        ret.append("}"); //$NON-NLS-1$
-
-        return ret.toString();
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void onSuccess(String result) {
-        String json = EventJSONFactory.build(EventJSONFactory.ActionType.SAVE_AS, buildPayload(result));
-
-        MessageDispatcher dispatcher = MessageDispatcher.getInstance();
-        dispatcher.processMessage(json);
-
-        wait.close();
-    }
-}

src/main/java/org/iplantc/de/client/services/RawDataServices.java
@@ -1,100 +1,0 @@
-package org.iplantc.de.client.services;
-
-import org.iplantc.de.client.models.DEProperties;
-import org.iplantc.de.shared.services.MultiPartServiceWrapper;
-import org.iplantc.de.shared.services.ServiceCallWrapper;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-/**
- * Provides access to remote services to acquire raw data.
- * 
- * @author amuir
- * 
- */
-public class RawDataServices {
-
-
-    /**
-     * Call service to retrieve the provenance data for a requested file
-     * 
-     * @param idFile file to retrieve provenance from.
-     * @param callback executes when RPC call is complete.
-     */
-    public static void getFileProvenance(String idFile, AsyncCallback<String> callback) {
-        String address = DEProperties.getInstance().getMuleServiceBaseUrl() + "files/" + idFile //$NON-NLS-1$
-                + "/provenance.text.all"; //$NON-NLS-1$
-        ServiceCallWrapper wrapper = new ServiceCallWrapper(address);
-
-        DEServiceFacade.getInstance().getServiceData(wrapper, callback);
-    }
-
-
-
-    /**
-     * Call service to save the raw data
-     * 
-     * @param idFile identifier of file to save.
-     * @param filename name of the file.
-     * @param body multi-part body.
-     * @param callback executes when RPC call is complete.
-     */
-    public static void saveRawData(String idFile, String filename, String body,
-            AsyncCallback<String> callback) {
-        String address = DEProperties.getInstance().getMuleServiceBaseUrl() + "files/" + idFile; //$NON-NLS-1$
-        MultiPartServiceWrapper wrapper = new MultiPartServiceWrapper(MultiPartServiceWrapper.Type.PUT,
-                address);
-
-        String disposition = "name=\"file\"; filename=\"" + filename + "\""; //$NON-NLS-1$ //$NON-NLS-2$
-        wrapper.addPart(body, disposition);
-
-        DEServiceFacade.getInstance().getServiceData(wrapper, callback);
-    }
-
-    /**
-     * Call service to save the raw data under a different name
-     * 
-     * @param idWorkspace unique id for user's workspace.
-     * @param idFolder parent folder identifier.
-     * @param idFile id of original file to be saved.
-     * @param filename new file name.
-     * @param body multi-part body.
-     * @param callback executes when RPC call is complete.
-     */
-    public static void saveAsRawData(String idWorkspace, String idFolder, String idFile,
-            String filename, String body, AsyncCallback<String> callback) {
-        String address = DEProperties.getInstance().getMuleServiceBaseUrl() + "workspaces/" //$NON-NLS-1$
-                + idWorkspace + "/folders/" + idFolder + "/files"; //$NON-NLS-1$ //$NON-NLS-2$
-        MultiPartServiceWrapper wrapper = new MultiPartServiceWrapper(MultiPartServiceWrapper.Type.POST,
-                address);
-
-        String disposition = "name=\"file\"; filename=\"" + filename + "\""; //$NON-NLS-1$ //$NON-NLS-2$
-        wrapper.addPart(body, disposition);
-
-        disposition = "name=\"copied-from\""; //$NON-NLS-1$
-        wrapper.addPart(idFile, disposition);
-
-        DEServiceFacade.getInstance().getServiceData(wrapper, callback);
-    }
-
-
-
-    /**
-     * Call service to save file description
-     * 
-     * @param idFile id of file to save.
-     * @param description new description.
-     * @param callback executes when RPC call is complete.
-     */
-    public static void saveFileDescription(String idFile, String description,
-            AsyncCallback<String> callback) {
-        String address = DEProperties.getInstance().getMuleServiceBaseUrl() + "files/" + idFile //$NON-NLS-1$
-                + "/description"; //$NON-NLS-1$
-        ServiceCallWrapper wrapper = new ServiceCallWrapper(ServiceCallWrapper.Type.PUT, address,
-                description);
-
-        DEServiceFacade.getInstance().getServiceData(wrapper, callback);
-    }
-
-
-}

src/main/java/org/iplantc/de/client/views/panels/DataProvenancePanel.java
@@ -1,38 +1,0 @@
-package org.iplantc.de.client.views.panels;
-
-import org.iplantc.de.client.I18N;
-import org.iplantc.de.client.services.RawDataServices;
-import org.iplantc.de.client.utils.ProvenanceFormatter;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public class DataProvenancePanel extends DataTextAreaPanel {
-    public DataProvenancePanel() {
-    }
-
-    @Override
-    protected String getHeadingText() {
-        return I18N.CONSTANT.provenance();
-    }
-
-    @Override
-    protected int getInitialHeight() {
-        return 80;
-    }
-
-    @Override
-    protected void updateDisplay(String idFile) {
-        RawDataServices.getFileProvenance(idFile, new AsyncCallback<String>() {
-            @Override
-            public void onFailure(Throwable arg0) {
-                hide();
-            }
-
-            @Override
-            public void onSuccess(String result) {
-                displayValue(ProvenanceFormatter.format(result));
-                show();
-            }
-        });
-    }
-}

src/main/java/org/iplantc/de/client/views/panels/RawDataSaveAsDialogPanel.java
@@ -1,57 +1,0 @@
-package org.iplantc.de.client.views.panels;
-
-import org.iplantc.core.uicommons.client.models.UserInfo;
-import org.iplantc.core.uicommons.client.views.panels.IPlantPromptPanel;
-import org.iplantc.core.uidiskresource.client.models.FileIdentifier;
-import org.iplantc.de.client.I18N;
-import org.iplantc.de.client.services.RawDataSaveAsCallback;
-import org.iplantc.de.client.services.RawDataServices;
-
-import com.extjs.gxt.ui.client.widget.MessageBox;
-
-/**
- * Provides a user interface for "save as" operations with raw data.
- */
-public class RawDataSaveAsDialogPanel extends IPlantPromptPanel {
-    protected FileIdentifier file;
-    protected String data;
-    private final MessageBox wait;
-
-    /**
-     * Instantiate from a file identifier, data and wait message box.
-     * 
-     * @param file file associated with this panel.
-     * @param data data to be saved.
-     * @param wait message box to display while saving.
-     */
-    public RawDataSaveAsDialogPanel(FileIdentifier file, String data, MessageBox wait) {
-        super(I18N.DISPLAY.fileName());
-
-        this.file = file;
-        this.data = data;
-        this.wait = wait;
-        field.setValue(file.getFilename());
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void handleOkClick() {
-        String name = field.getValue();
-
-        if (name != null) {
-            name = name.trim();
-
-            if (name.length() > 0) {
-                UserInfo uinfo = UserInfo.getInstance();
-                // temp strings for readability
-                String idParent = file.getParentId();
-                String idFile = file.getFileId();
-                String idWorkspace = uinfo.getWorkspaceId();
-                RawDataServices.saveAsRawData(idWorkspace, idParent, idFile, field.getValue(), data,
-                        new RawDataSaveAsCallback(idParent, idFile, wait));
-            }
-        }
-    }
-}

src/main/java/org/iplantc/de/client/views/windows/FileEditorWindow.java
@@ -351,7 +351,7 @@ public class FileEditorWindow extends ProvenanceWindow implements DataMonitor {
         @Override
         public void addTab(String result) {
             // add a raw data tab
-            RawDataPanel panelRaw = new RawDataPanel(file, result, false);
+            RawDataPanel panelRaw = new RawDataPanel(file, result);
             panelRaw.setTabIndex(treeUrlTabIndex == 0 ? 1 : 0);
             panel.addTab(panelRaw, provenance);
         }

src/main/java/org/iplantc/de/client/views/windows/ProvenanceWindow.java
@@ -1,11 +1,8 @@
 package org.iplantc.de.client.views.windows;
 
 import org.iplantc.core.uidiskresource.client.models.FileIdentifier;
-import org.iplantc.de.client.services.RawDataServices;
-import org.iplantc.de.client.utils.ProvenanceFormatter;
 
 import com.extjs.gxt.ui.client.widget.layout.FitLayout;
-import com.google.gwt.user.client.rpc.AsyncCallback;
 
 /**
  * Provides a user interface for provenance widgets.
@@ -54,24 +51,6 @@ public abstract class ProvenanceWindow extends FileWindow {
 
         // add necessary tabs
         constructPanel();
-    }
-
-    /**
-     * Retrieve and update provenance information.
-     */
-    protected void updateProvenance() {
-        // retrieve provenance from the server
-        RawDataServices.getFileProvenance(file.getFileId(), new AsyncCallback<String>() {
-            @Override
-            public void onFailure(Throwable caught) {
-                // do nothing if there is no provenance data
-            }
-
-            @Override
-            public void onSuccess(String result) {
-                updateProvenance(ProvenanceFormatter.format(result));
-            }
-        });
     }
 
     /**

src/main/java/org/iplantc/de/server/StringEnumUserType.java
@@ -1,196 +1,0 @@
-package org.iplantc.de.server;
-
-import java.io.Serializable;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.Properties;
-
-import org.hibernate.Hibernate;
-import org.hibernate.HibernateException;
-import org.hibernate.usertype.EnhancedUserType;
-import org.hibernate.usertype.ParameterizedType;
-import org.hibernate.util.ReflectHelper;
-
-/**
- * A generic UserType that handles String-based JDK 5.0 Enums.
- * 
- * @author Gavin King
- */
-public class StringEnumUserType implements EnhancedUserType, ParameterizedType {
-
-    @SuppressWarnings("rawtypes")
-    private Class<Enum> enumClass;
-
-    /**
-     * Sets the parameter values for the properties specified.
-     * 
-     * @param parameters collection of parameter values.
-     */
-    @SuppressWarnings("unchecked")
-    public void setParameterValues(Properties parameters) {
-        String enumClassName = parameters.getProperty("enumClassname"); //$NON-NLS-1$
-        try {
-            enumClass = ReflectHelper.classForName(enumClassName);
-        } catch (ClassNotFoundException cnfe) {
-            throw new HibernateException("Enum class not found", cnfe); //$NON-NLS-1$
-        }
-    }
-
-    /**
-     * Returns a class object for the underlying enum representation.
-     * 
-     * @return an instance of the underlying enum representation.
-     */
-    @SuppressWarnings("rawtypes")
-    public Class returnedClass() {
-        return enumClass;
-    }
-
-    /**
-     * Gets the supported SQL types for the enum.
-     * 
-     * @return an array of supported SQL types.
-     */
-    public int[] sqlTypes() {
-        return new int[] {Hibernate.STRING.sqlType()};
-    }
-
-    /**
-     * Indicates if the enum is mutable.
-     * 
-     * @return false if the enum is not modifiable; otherwise true.
-     */
-    public boolean isMutable() {
-        return false;
-    }
-
-    /**
-     * Performs a deep copy.
-     * 
-     * This implementation is merely a pass-through implementation.
-     * 
-     * @param value object to copy.
-     * @return a reference to the new copy.
-     */
-    public Object deepCopy(Object value) {
-        return value;
-    }
-
-    /**
-     * Breaks down the object into a serializable representation.
-     * 
-     * @param value the object to serialize.
-     * @return a serializable representation of the object.
-     */
-    @SuppressWarnings("rawtypes")
-    public Serializable disassemble(Object value) {
-        return (Enum)value;
-    }
-
-    /**
-     * Replaces the original with the target.
-     * 
-     * This implementation merely returns the 'original' argument.
-     * 
-     * @param original the original object representation.
-     * @param target the target object to replace.
-     * @param owner the owner of the target.
-     * @return a reference to the original object.
-     */
-    public Object replace(Object original, Object target, Object owner) {
-        return original;
-    }
-
-    /**
-     * Builds up an object representation.
-     * 
-     * This implementation merely passes along the 'cached' argument.
-     * 
-     * @param cached the cached serializable representation
-     * @param owner the owner of the target.
-     * @return a reference to the cached object.
-     */
-    public Object assemble(Serializable cached, Object owner) {
-        return cached;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public boolean equals(Object x, Object y) {
-        return x == y;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public int hashCode(Object x) {
-        return x.hashCode();
-    }
-
-    /**
-     * Create a enum from an XML string representation.
-     * 
-     * @param xmlValue a valid XML representation.
-     * @return an enum representation of the value.
-     */
-    @SuppressWarnings("unchecked")
-    public Object fromXMLString(String xmlValue) {
-        return Enum.valueOf(enumClass, xmlValue);
-    }
-
-    /**
-     * Convert from an object representation to a SQL string representation.
-     * 
-     * @param value a valid object representation.
-     * @return a valid SQL string representation of the value.
-     */
-    @SuppressWarnings("rawtypes")
-    public String objectToSQLString(Object value) {
-        return '\'' + ((Enum)value).name() + '\'';
-    }
-
-    /**
-     * Returns an XML string representation of the value.
-     * 
-     * @param value a valid object representation.
-     * @return an XML string representation of the value.
-     */
-    @SuppressWarnings("rawtypes")
-    public String toXMLString(Object value) {
-        return ((Enum)value).name();
-    }
-
-    /**
-     * Performs a safe "get" operation to retrieve the enum value.
-     * 
-     * @param rs a database result set from a query.
-     * @param names an array of names, expected to be a single element.
-     * @param owner the owner of the object.
-     * @return a reference to the enum if successful; otherwise null.
-     * @throws SQLException can be generated by interaction with the result set.
-     */
-    @SuppressWarnings("unchecked")
-    public Object nullSafeGet(ResultSet rs, String[] names, Object owner) throws SQLException {
-        String name = rs.getString(names[0]);
-        return rs.wasNull() ? null : Enum.valueOf(enumClass, name);
-    }
-
-    /**
-     * Performs a safe "set" operation to modify the enum value in a prepared statement.
-     * 
-     * @param st the prepared statement to modify.
-     * @param value the enum value to set.
-     * @param index the ordeal index to set within the prepared statement.
-     * @throws SQLException can be generated by interaction with the prepared statement.
-     */
-    @SuppressWarnings("rawtypes")
-    public void nullSafeSet(PreparedStatement st, Object value, int index) throws SQLException {
-        if (value == null) {
-            st.setNull(index, Hibernate.STRING.sqlType());
-        } else {
-            st.setString(index, ((Enum)value).name());
-        }
-    }
-}

commit 0cf42fc8d76380a41579422300b3179bd0f9f94c

Fixed RawDataPanel null-ptr exception.
Fixed error introduced by commit d4106034acb9695d8540e66fc62d061008170925.

src/main/java/org/iplantc/de/client/views/panels/RawDataPanel.java
@@ -49,6 +49,7 @@ public class RawDataPanel extends ProvenanceContentPanel implements DataMonitor 
     }
 
     private void buildTextArea() {
+        areaData = buildTextArea(false);
         areaData.setId("idRawDataField"); //$NON-NLS-1$
     }
 