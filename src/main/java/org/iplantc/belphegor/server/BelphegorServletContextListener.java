20274b6f60a6e311c7b06523260d3a0d32381153
Dennis Roberts <dennis@iplantcollaborative.org>
2012-09-13 16:44:13 -0700

    CORE-3495: moved the registration of the session initialization servlet to BelphegorServletContextListener

--- src/main/java/org/iplantc/belphegor/server/BelphegorServletContextListener.java
+++ src/main/java/org/iplantc/belphegor/server/BelphegorServletContextListener.java
@@ -1,9 +1,9 @@
 package org.iplantc.belphegor.server;
 
 import java.util.Properties;
-import org.apache.log4j.Logger;
-import org.iplantc.de.server.spring.AbstractServletContextListener;
-import org.iplantc.de.server.spring.ClavinPropertyPlaceholderConfigurer;
+import org.iplantc.clavin.spring.AbstractServletContextListener;
+import org.iplantc.clavin.spring.ClavinPropertyPlaceholderConfigurer;
+import org.iplantc.de.server.CasSessionInitializationServlet;
 
 /**
  * A ServletContextListener written specifically for Belphegor.
@@ -11,11 +11,6 @@ import org.iplantc.de.server.spring.ClavinPropertyPlaceholderConfigurer;
  * @author Dennis Roberts
  */
 public class BelphegorServletContextListener extends AbstractServletContextListener {
-
-    /**
-     * Used to log configuration settings.
-     */
-    private static final Logger LOG = Logger.getLogger(BelphegorServletContextListener.class);
 
     /**
      * The Belphegor configuration settings.
@@ -26,6 +21,14 @@ public class BelphegorServletContextListener extends AbstractServletContextListe
      * The Confluence client configuration settings.
      */
     private Properties confluenceConfig;
+
+    /**
+     * Servlet registration information.
+     */
+    private ServletRegistrationInfo[] regInfo = {
+        new ServletRegistrationInfo(new CasSessionInitializationServlet("#workspace"), "sessionInitializationServlet",
+            "/belphegor/login")
+    };
 
     /**
      * Loads the configuration settings.
@@ -39,8 +42,12 @@ public class BelphegorServletContextListener extends AbstractServletContextListe
     }
 
     /**
-     * Registers the servlets that are used by Belphegor.
+     * Obtains servlet registration information.
+     *
+     * @return the servlet registration information.
      */
     @Override
-    protected void registerServlets() {}
+    public ServletRegistrationInfo[] getServletRegistrationInfo() {
+        return regInfo;
+    }
 }
