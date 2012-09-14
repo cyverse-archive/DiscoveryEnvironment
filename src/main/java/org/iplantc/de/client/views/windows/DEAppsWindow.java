8a13bf5dbc094ba56bc4d2d988e361f6ae8e9cc6
Jonathan Strootman <jstroot@iplancollaborative.org>
2012-09-14 13:05:33 -0700

    Changed all occurrences of the *.shared.GWT class to *.client.GWT Updated pom to specify war, clean, and compile plugin versions Removed unnecessary "replace-with" in gwt.xml

--- src/main/java/org/iplantc/de/client/views/windows/DEAppsWindow.java
+++ src/main/java/org/iplantc/de/client/views/windows/DEAppsWindow.java
@@ -1,12 +1,11 @@
 package org.iplantc.de.client.views.windows;
 
 import org.iplantc.core.uiapplications.client.CommonAppDisplayStrings;
-import org.iplantc.core.uiapplications.client.I18N;
 import org.iplantc.core.uiapplications.client.models.CatalogWindowConfig;
 import org.iplantc.core.uiapplications.client.models.autobeans.Analysis;
 import org.iplantc.core.uiapplications.client.models.autobeans.AnalysisGroup;
 import org.iplantc.core.uiapplications.client.presenter.AppsViewPresenter;
-import org.iplantc.core.uiapplications.client.services.AppTemplateServiceFacade;
+import org.iplantc.core.uiapplications.client.services.AppTemplateUserServiceFacade;
 import org.iplantc.core.uiapplications.client.views.AnalysisColumnModel;
 import org.iplantc.core.uiapplications.client.views.AppsView;
 import org.iplantc.core.uiapplications.client.views.AppsViewImpl;
@@ -14,6 +13,7 @@ import org.iplantc.core.uicommons.client.events.EventBus;
 import org.iplantc.core.uicommons.client.models.UserInfo;
 import org.iplantc.core.uicommons.client.models.WindowConfig;
 import org.iplantc.de.client.Constants;
+import org.iplantc.de.client.I18N;
 import org.iplantc.de.client.dispatchers.WindowDispatcher;
 import org.iplantc.de.client.factories.EventJSONFactory.ActionType;
 import org.iplantc.de.client.factories.WindowConfigFactory;
@@ -34,22 +34,23 @@ public class DEAppsWindow extends Gxt3IplantWindow {
         super(tag, config);
         // FIXME JDS Use dependency injection to get the classes needed in the constructor.
         CommonAppDisplayStrings commonAppDisplayStrings = org.iplantc.core.uiapplications.client.I18N.DISPLAY;
-        CommonAppDisplayStrings deDisplayStrings = I18N.DISPLAY;
         EventBus eventBus = EventBus.getInstance();
-        AppTemplateServiceFacade templateService = GWT.create(AppTemplateServiceFacade.class);
+        AppTemplateUserServiceFacade templateService = GWT.create(AppTemplateUserServiceFacade.class);
         UserInfo userInfo = UserInfo.getInstance();
 
         TreeStore<AnalysisGroup> treeStore = new TreeStore<AnalysisGroup>(
                 new AnalysisGroupModelKeyProvider());
         ListStore<Analysis> listStore = new ListStore<Analysis>(new AnalysisModelKeyProvider());
-        AnalysisColumnModel cm = new AnalysisColumnModel(eventBus, commonAppDisplayStrings);
+        AnalysisColumnModel cm = new AnalysisColumnModel(eventBus, commonAppDisplayStrings,
+                templateService);
 
         AppsView view = new AppsViewImpl(treeStore, listStore, cm);
-        presenter = new AppsViewPresenter(view, templateService, deDisplayStrings, userInfo,
+        presenter = new AppsViewPresenter(view, templateService, commonAppDisplayStrings, userInfo,
                 (CatalogWindowConfig)config);
 
         setSize("800", "410");
         presenter.go(this);
+        setHeadingText(I18N.DISPLAY.applications());
     }
 
     private final class AnalysisGroupModelKeyProvider implements ModelKeyProvider<AnalysisGroup> {
@@ -103,25 +104,19 @@ public class DEAppsWindow extends Gxt3IplantWindow {
 
     @Override
     public void setWindowConfig(WindowConfig config) {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void setMinimized(boolean min) {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public boolean isMinimized() {
-        // TODO Auto-generated method stub
         return false;
     }
 
     @Override
     public int getHeaderOffSetHeight() {
-        // TODO Auto-generated method stub
         return 0;
     }
 
