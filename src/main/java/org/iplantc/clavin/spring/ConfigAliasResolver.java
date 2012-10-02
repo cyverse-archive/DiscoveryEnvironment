6f7c9b3815f6418910121a2c6faa44fdfe9c838d
Dennis Roberts <dennis@iplantcollaborative.org>
2012-10-01 17:45:54 -0700

    CORE-3495: added some static methods to look up configurations

--- src/main/java/org/iplantc/clavin/spring/ConfigAliasResolver.java
+++ src/main/java/org/iplantc/clavin/spring/ConfigAliasResolver.java
@@ -2,6 +2,9 @@ package org.iplantc.clavin.spring;
 
 import java.util.Map;
 import java.util.Properties;
+import javax.servlet.ServletContext;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 /**
  * Resolves aliased configurations names.
@@ -43,5 +46,51 @@ public class ConfigAliasResolver {
     public Properties getAliasedConfig(String alias) {
         String name = aliases.get(alias);
         return name == null ? null : configurer.getConfig(name);
+    }
+
+    /**
+     * Retrieves an aliased configuration, throwing an {@link IllegalStateException} if a configuration with the
+     * given alias can't be found.
+     *
+     * @param alias the configuration alias.
+     * @return the configuration.
+     * @throws IllegalStateException if the configuration isn't found.
+     */
+    public Properties getRequiredAliasedConfig(String alias) {
+        Properties result = getAliasedConfig(alias);
+        if (result == null) {
+            throw new IllegalStateException("no configuration with alias, " + alias + ", found");
+        }
+        return result;
+    }
+
+    /**
+     * Retrieves an aliased configuration for a servlet context.
+     *
+     * @param context the servlet context.
+     * @param alias the configuration alias.
+     * @return the configuration or null if the configuration isn't found.
+     */
+    public static Properties getAliasedConfigFrom(ServletContext context, String alias) {
+        WebApplicationContext appContext = WebApplicationContextUtils.getRequiredWebApplicationContext(context);
+        ConfigAliasResolver resolver = appContext.getBean(ConfigAliasResolver.class);
+        return resolver.getAliasedConfig(alias);
+    }
+
+    /**
+     * Retrieves an aliased configuration for a servlet context, throwing an {@link IllegalStateException} if a
+     * configuration with the given alias isn't found.
+     *
+     * @param context the servlet context.
+     * @param alias the configuration alias.
+     * @return the configuration.
+     * @throws IllegalStateException if the configuration isn't found.
+     */
+    public static Properties getRequiredAliasedConfigFrom(ServletContext context, String alias) {
+        Properties result = getAliasedConfigFrom(context, alias);
+        if (result == null) {
+            throw new IllegalStateException("no configuration with alias, " + alias + ", found");
+        }
+        return result;
     }
 }
